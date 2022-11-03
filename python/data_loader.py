30e70f0727210c527a412bd4360511ed90afd886
Brett Nelson <43356326+throneofshadow@users.noreply.github.com>
2022-11-03 16:40:39 -0700

    Added query method for GUI use, added conditional queries (handedness, tug of war, number of grasps initially)

--- python/data_loader.py
+++ python/data_loader.py
@@ -10,7 +10,7 @@ def import_file(path_to_csv, use_index=False):
 
 
 class BlockQuery:
-    def __init__(self, dir_path, rat, date, session, fps=10):
+    def __init__(self, dir_path, rat, date, session, query_list, fps=10):
         self.cap, self.data, self.prediction_file = None, None, None
         self.single_reach_video_images, self.single_reach_query_data, self.single_reach_query_predictions = None, None, None
         self.load_path, self.fps, self.video_width, self.video_height, self.fps = None, None, None, None, fps
@@ -18,6 +18,7 @@ class BlockQuery:
         self.rat = rat
         self.date = date
         self.session = session
+        self.query_types = query_list
         self.load_data_from_main_path()
 
     def load_data_from_main_path(self):
@@ -37,7 +38,7 @@ class BlockQuery:
         self.data = import_file(data_path)
 
     def query_over_block(self, hand_query=False, grasp_query=False, tug_query=False):
-        row_= 0
+        row_ = 0
         for row in self.prediction_file.iterrows():
             if row[1][1] > 0:
                 lb = row[1][1]
@@ -46,13 +47,24 @@ class BlockQuery:
                 ind_grasps = grasps.split(',')
                 num_grasps = len(ind_grasps)
                 hand = row[1][4]
+                if 'l' or 'L' in hand:
+                    hand = 1
+                else:
+                    hand = 0
                 tug = row[1][5]
                 notes = row[1][6]
                 row_ += 1
                 if notes:
                     break
-                else:
-                    # Extract data if conditions are met (use hand, tug, and num_grasps)
+                elif self.query_types[0] and num_grasps > int(num_grasps):
+                    self.load_data_from_boundaries(lb, ub, row_)
+                elif self.query_types[1] and hand < 1:
+                    self.load_data_from_boundaries(lb, ub, row_)
+                elif self.query_types[1] and hand > 0:
+                    self.load_data_from_boundaries(lb, ub, row_)
+                elif self.query_types[2] and tug > 0:  # Tug of war condition
+                    self.load_data_from_boundaries(lb, ub, row_)
+                else:  # Empty query, just load individual reaches
                     self.load_data_from_boundaries(lb, ub, row_)
                     # Do things here (plot data, save a cut video into a directory named reaches)
 
@@ -83,8 +95,8 @@ class BlockQuery:
         return np.stack(images, axis=0)
 
     def cut_reaching_video(self, reach_number, fps):
-        if not os.path.exists(self.load_path+'\\classification_videos\\trial_videos'):
-            os.makedirs(self.load_path+'\\classification_videos\\trial_videos')
+        if not os.path.exists(self.load_path + '\\classification_videos\\trial_videos'):
+            os.makedirs(self.load_path + '\\classification_videos\\trial_videos')
         out = cv2.VideoWriter('reach_' + str(reach_number) + 'video.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps,
                               (self.video_width, self.video_height), False)
         for i in range(self.single_reach_video_images.shape[0]):
@@ -96,17 +108,24 @@ class BlockQuery:
 
     def find_end_time(self):
         """ Function to find the end time of a given reach (from trajectory minimization) """
+
     def find_start_time(self):
         """ Function to find the tentative start time of a reach, given a definite positive threshold on a given palm. """
+
     def find_duration(self):
         """ Function to find the tentative duration of a reach, given a definite positive threshold on a given palm. """
+
     def find_grasp_peak(self):
         """ Find peak time of a given grasp using a palm speed thresholding. """
+
     def find_retract_times(self):
         """ Find total time to retract"""
+
     def find_retract_start(self):
         """ Find time retraction starts using handle data. """
+
     def find_retract_stop(self):
         """ Find time when retraction stops using handle data. """
+
     def find_lick_times(self):
         """ Find tentative reward-delivering times using a lick-o-meter sensor. """
